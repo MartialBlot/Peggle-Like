74290c494363da7be68c3a4bcdb9ce3fe1ea95e5
BLOT Martial <martial.blot@outlook.fr>
2019-07-09 16:38:04 +0200

    return to mouse control

--- index.js
+++ index.js
@@ -3,76 +3,41 @@ let canvas = document.getElementById('game');
 let ctx = canvas.getContext('2d');
 
 //Ball options
-let x = 500;
-let y = 40;
+let x = 600;
+let y = 60;
 let startAngle = 10;
 let endAngle = 0;
 let anticlockwise = Math.PI*2;
 
-//Keyboard controls
-let keyState = {};    
-document.addEventListener('keydown',function(e){
-    keyState[e.keyCode || e.which] = true;
-},true);    
-document.addEventListener('keyup',function(e){
-    keyState[e.keyCode || e.which] = false;
-},true);
+//Mouse controls
+canvas.addEventListener("mousemove", mouseMouvement);
 
-let dirArrowX = 500;
+let dirArrowX = 600;
 let dirArrowY = 100;
-let refShooter = 500;
-
-//Arrow option with mouse -- not yet
-//Arrow option with mouse -- not yet
-//canvas.addEventListener("mousemove", mouseMouvement);
-//function mouseMouvement(event){
-//       dirArrowX = event.pageX;
-//}
+let refShooter = 600;
 
 function draw(){
 
-ctx.clearRect(0, 0, 900, 600)
+ctx.clearRect(0, 0, 1200, 600)
 
 ctx.beginPath();
 ctx.arc(x, y, startAngle, endAngle, anticlockwise);
 ctx.fill();
 
 ctx.beginPath();
-ctx.arc(500, 0, 60, 0, anticlockwise);
+ctx.arc(600, 0, 60, 0, anticlockwise);
 ctx.stroke();
 
 ctx.beginPath();
-ctx.moveTo(500, 60);
+ctx.moveTo(600, 60);
 ctx.lineTo(dirArrowX, dirArrowY);
 ctx.stroke();
 
 //Controls
-
-// right
-if (keyState[39] && dirArrowY >= 60){
-    if(dirArrowX>=refShooter){
-        dirArrowX+=1
-        dirArrowY-=1
+function mouseMouvement(event){
+    dirArrowX = event.pageX;
+    dirArrowY = event.pageY;
     }
-    else{
-        dirArrowX+=1
-        dirArrowY+=1
-    }
-}
-
-//left
-if (keyState[37]){
-    if(dirArrowX>=refShooter){
-        dirArrowX-=1
-        dirArrowY+=1
-    }
-    else{
-        dirArrowX-=1
-        dirArrowY-=1
-    }
-}
-console.log(dirArrowY, dirArrowX)
-
 }
 draw()
 
